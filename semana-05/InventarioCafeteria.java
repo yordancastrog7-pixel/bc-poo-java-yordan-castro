ec85f713a5d9e47db5309b9077be31f1577e4c2f
yordancastrog7-pixel <yordancastrog7@gmail.com>
2025-11-25 16:23:15 -0500

    feat(semana-05): actualizar inventario y main

--- semana-05/InventarioCafeteria.java
+++ semana-05/InventarioCafeteria.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class InventarioCafeteria {
-    
+
+    private ArrayList<Producto> productos;
+
+    public InventarioCafeteria() {
+        productos = new ArrayList<>();
+    }
+
+    // --- SOBRECARGA ---
+    // Agregar un producto
+    public void agregarProducto(Producto p) {
+        productos.add(p);
+    }
+
+    // Agregar varios productos
+    public void agregarProducto(Producto p1, Producto p2) {
+        productos.add(p1);
+        productos.add(p2);
+    }
+
+    // --- POLIMORFISMO DINÁMICO ---
+    public void mostrarInventario() {
+        for (Producto p : productos) {
+            p.mostrarInfo();
+            System.out.println("Precio final: $" + p.calcularPrecio());
+            System.out.println("----------------------");
+        }
+    }
 }
--- semana-05/Main.java
+++ semana-05/Main.java
@@ -1,3 +1,24 @@
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        InventarioCafeteria inventario = new InventarioCafeteria();
+
+        Producto p1 = new Bebida("B001", "Capuccino", 6000, false);
+        Producto p2 = new Bebida("B002", "Jugo de Naranja", 5000, true);
+        Producto p3 = new Postre("P001", "Torta Chocolate", 4000, 2);
+        Producto p4 = new Postre("P002", "Cheesecake", 5000, 1);
+        Producto p5 = new Bebida("B003", "Té Helado", 4500, true);
+
+        // Sobrecarga
+        inventario.agregarProducto(p1);
+        inventario.agregarProducto(p2, p3);
+
+        // Normales
+        inventario.agregarProducto(p4);
+        inventario.agregarProducto(p5);
+
+        // Mostrar inventario
+        inventario.mostrarInventario();
+    }
 }
